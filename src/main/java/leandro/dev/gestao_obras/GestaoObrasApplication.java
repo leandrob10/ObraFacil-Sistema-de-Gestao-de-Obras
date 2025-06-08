6602cfef08157c667f122f3d56027afaf6847342
leandro barreto <leandrobarreto.barreto321@gmail.com>
2025-06-08 14:12:31 -0300

    initial - Projeto Gestao de obras

---
+++ src/main/java/leandro/dev/gestao_obras/GestaoObrasApplication.java
@@ -0,0 +1,13 @@
+package leandro.dev.gestao_obras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoObrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoObrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leandro/dev/gestao_obras/GestaoObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package leandro.dev.gestao_obras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
