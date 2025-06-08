cad3229ba0b7b5259bc137c980111d42a5224226
leandro barreto <leandrobarreto.barreto321@gmail.com>
2025-06-08 14:17:00 -0300

    model - adicionar entidade ChecklistItem

---
+++ src/main/java/leandro/dev/gestao_obras/model/ChecklistItem.java
@@ -0,0 +1,16 @@
+package leandro.dev.gestao_obras.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ckeclist_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChecklistItem {
+
+}
